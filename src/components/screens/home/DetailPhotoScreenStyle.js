16eb854576e7e2ad4ab81b602e0484c1ed97a271
Jay Zhou <hollowic@hotmail.com>
2020-02-19 16:52:22 -0800

    Smooooth as butter modal =)

--- src/components/screens/home/DetailPhotoScreenStyle.js
+++ src/components/screens/home/DetailPhotoScreenStyle.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,6 +10,33 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "50%"
+  },
+  icon: {
+    height: 45,
+    width: 45,
+    borderRadius: 50
+  },
+  thumbnail: { height: 100, width: 100 },
+  botContainer: {
+    width: "100%",
+    height: "50%"
+  },
+  botHeader: {
+    width: Dimensions.get("window").width,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  direction: {
+    alignItems: "center"
+  },
+  info: {
+    padding: 10
+  },
+  expanded: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
   }
 });
 
