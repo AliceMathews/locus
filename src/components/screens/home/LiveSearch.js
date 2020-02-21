ad004ff4f88b2b3a3305cffddabc6b86d2c2f7ff
edison-cy-yang <edison.cy.yang@gmail.com>
2020-02-20 20:02:04 -0800

    platform specific searchbar input

--- src/components/screens/home/LiveSearch.js
+++ src/components/screens/home/LiveSearch.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Platform } from "react-native";
 import { SearchBar } from "react-native-elements";
 
 import styles from "./LiveSearchStyle";
 
 export default function LiveSearch(props) {
   return (
-      <SearchBar
+    <View style={styles.searchBarContainer}>
+      {Platform.OS === 'ios' && (
+        <SearchBar
+          platform="ios"
           placeholder="Search here..."
-          lightTheme
-          round
           onChangeText={props.onChangeText}
           value={props.value}
-          showCancel={true}
+          showCancel
           containerStyle={{width: props.width}}
           showLoading={props.loading}
-      />
+        />
+      )}
+      {Platform.OS === 'android' && (
+        <SearchBar
+          platform="android"
+          placeholder="Search here..."
+          onChangeText={props.onChangeText}
+          value={props.value}
+          cancelIcon
+          containerStyle={{width: props.width}}
+          showLoading={props.loading}
+        />
+      )}
+    </View>
   );
-}
+};
