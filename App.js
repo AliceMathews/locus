986f979f6a9f448e504e60d359d21f83f0a19841
Jay Zhou <hollowic@hotmail.com>
2020-02-20 19:16:40 -0800

    cleaning up app and preping to move over to hook

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useReducer, useMemo } from "react";
-import { View, Image, AsyncStorage, Alert } from "react-native";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { AsyncStorage, Alert } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AppLoading } from "expo";
 import { MaterialIcons } from "@expo/vector-icons";
-import axios from "axios";
 import { API_URL } from "./configKeys";
+import axios from "axios";
 
 import HomeStackNav from "./src/components/stackNavigators/HomeStackNav";
 import UploadStackNav from "./src/components/stackNavigators/UploadStackNav";
@@ -13,10 +13,9 @@ import UserStackNav from "./src/components/stackNavigators/UserStackNav";
 import SplashScreen from "./src/components/screens/splash/Splash";
 
 const Tab = createBottomTabNavigator();
-const AuthContext = React.createContext();
+// const AuthContext = React.createContext();
 
-export default function App({ navigation }) {
-  // const navigation = useNavigation();
+export default function App() {
   const [state, dispatch] = useReducer(
     (prevState, action) => {
       switch (action.type) {
@@ -35,7 +34,7 @@ export default function App({ navigation }) {
         case "SIGN_OUT":
           return {
             ...prevState,
-            isSignout: true,
+            isSignout: logout(),
             userToken: null
           };
       }
@@ -48,7 +47,6 @@ export default function App({ navigation }) {
   );
 
   useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
     const bootstrapAsync = async () => {
       let userToken;
 
@@ -70,12 +68,7 @@ export default function App({ navigation }) {
   const authContext = useMemo(
     () => ({
       signIn: async data => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
         try {
-          console.log("what is data", data);
           const res = await axios.post(`${API_URL}users/login`, {
             username: data.username,
             password: data.password
@@ -91,10 +84,7 @@ export default function App({ navigation }) {
       },
       signOut: () => dispatch({ type: "SIGN_OUT" }),
       signUp: async data => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
+        console.log(data);
         try {
           const res = await axios.post(`${API_URL}users/register`, {
             username: data.username,
@@ -148,17 +138,6 @@ export default function App({ navigation }) {
   );
 }
 
-// const [currentSession, setCurrentSession] = useState(null);
-// const [isLoading, setIsLoading] = useState(true);
-// const []
-// console.log("arriving in App, currentSession is", currentSession);
-// useEffect(() => {
-//   getToken();
-//   return () => {
-//     // cleanup;
-//   };
-// }, []);
-
 const storeToken = async token => {
   try {
     await AsyncStorage.setItem("userToken", JSON.stringify(token));
@@ -177,31 +156,30 @@ const storeToken = async token => {
 //   }
 // };
 
-// const logoutBackend = async () => {
-//   console.log("starting backend logout");
-//   try {
-//     let userData = await AsyncStorage.getItem("userData");
-//     let data = JSON.parse(userData);
-//     const res = await axios.post(`${API_URL}users/logout`, {
-//       data
-//     });
-//     console.log(res);
-//   } catch (err) {
-//     console.log("Something went wrong", error);
-//   }
-// };
+const logoutBackend = async () => {
+  console.log("starting backend logout");
+  try {
+    let userData = await AsyncStorage.getItem("userToken");
+    let data = JSON.parse(userData);
+    const res = await axios.post(`${API_URL}users/logout`, {
+      data
+    });
+    console.log(res);
+  } catch (err) {
+    console.log("Something went wrong", error);
+  }
+};
 
-// const logoutFrontend = async () => {
-//   console.log("starting frontend logout");
-//   try {
-//     await AsyncStorage.removeItem("userData");
-//     console.log("Auth token removed");
-//     navigation.navigate("Login", { amLoggedIn: false });
-//   } catch (err) {
-//     console.log("Something went wrong", error);
-//   }
-// };
+const logoutFrontend = async () => {
+  console.log("starting frontend logout");
+  try {
+    await AsyncStorage.removeItem("userToken");
+    console.log("Auth token removed");
+  } catch (err) {
+    console.log("Something went wrong", error);
+  }
+};
 
-// const logout = async () => {
-//   return Promise.all([logoutBackend(), logoutFrontend()]);
-// };
+const logout = async () => {
+  return Promise.all([logoutBackend(), logoutFrontend()]);
+};
