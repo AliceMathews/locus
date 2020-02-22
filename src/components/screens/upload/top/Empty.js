0c56a5bfdc2dc2f9e41ed0c20d8b75c0cdbda3bf
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-22 14:04:18 -0800

    updated styling for empty component

--- src/components/screens/upload/top/Empty.js
+++ src/components/screens/upload/top/Empty.js
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+
+import styles from "./EmptyStyle";
 
 export default function EmptyTop(props) {
   return (
     <>
-      <MaterialIcons
-        name={"add-circle"}
-        size={40}
-        color={"blue"}
-        onPress={() => props.press()}
-      />
-      <MaterialIcons
-        name={"add-a-photo"}
-        size={40}
-        color={"blue"}
-        onPress={() => {
-          props.pressCam();
-        }}
-      />
+      <View style={styles.buttons}>
+        <View style={styles.iconContainer}>
+          <MaterialIcons
+            name={"library-add"}
+            size={40}
+            color={"grey"}
+            onPress={() => props.press()}
+          />
+        </View>
+        <Text style={styles.text}>Open Library</Text>
+      </View>
+      <View style={styles.buttons}>
+        <View style={styles.iconContainer}>
+          <MaterialIcons
+            name={"add-a-photo"}
+            size={36}
+            color={"grey"}
+            onPress={() => {
+              props.pressCam();
+            }}
+          />
+        </View>
+
+        <Text style={styles.text}>Take Photo</Text>
+      </View>
     </>
   );
 }
