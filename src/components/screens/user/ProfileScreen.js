c0488c743ec79438fabcb8f450f582f4d24b4393
edison-cy-yang <edison.cy.yang@gmail.com>
2020-02-21 20:41:42 -0800

    display images that belong to a user

--- src/components/screens/user/ProfileScreen.js
+++ src/components/screens/user/ProfileScreen.js
@@ -1,38 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Image, Button } from "react-native";
+import { Text, View, Image, Button, ActivityIndicator, FlatList, Dimensions } from "react-native";
 import styles from "./ProfileScreenStyle";
 import { API_URL } from "../../../../configKeys";
 import axios from "axios";
 
-export default function ProfileScreen({ signOut, user, token }) {
+import PhotoTile from '../home/PhotoTile';
+
+export default function ProfileScreen({ signOut, user, token, navigation }) {
   const [currentUser, setCurrentUser] = useState(null);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     axios.get(`${API_URL}users/myinfo`, {headers: {"authorization": token}}).then(res => {
       setCurrentUser(res.data);
-      console.log(res.data);
+      const userId = res.data.id;
+      axios.get(`${API_URL}users/${userId}/images`).then(res => {
+        setImages(res.data);
+        setLoading(false);
+      })
     });
   }, []);
+  console.log(images);
+
+  const deviceWidth = Dimensions.get("window").width;
+
   return (
-    <View style={styles.container}>
-      {false && (
-        <>
-          <Image
-            source={{ uri: user.profile_pic }}
-            style={{ width: 300, height: 300 }}
-          />
-          <Text>{user.username}</Text>
-          <Button title="Sign out" onPress={signOut}></Button>
-        </>
-      )}
+    <View style={{flex: 1}}>
       {currentUser && (
-        <View>
-          <Image
-            source={{uri: currentUser.profile_pic}}
-            style={{width: 100, height: 100}} 
-          />
-          <Text style={{textAlign: 'center'}}>{currentUser.username}</Text>
-          <Button title="Sign out" onPress={signOut}></Button>
-        </View>
+        <>
+          <View style={styles.profileInfoContainer}>
+            <Image
+              source={{uri: currentUser.profile_pic}}
+              style={{width: 80, height: 80}} 
+            />
+            <Text style={{textAlign: 'center'}}>{currentUser.username}</Text>
+            <Button title="Sign out" onPress={signOut}></Button>
+          </View>
+          <View style={styles.userPhotosContainer}>
+            {loading && (
+              <ActivityIndicator size="large" color="#0000ff" />
+            )}
+            {!loading && (
+              <FlatList
+                numColumns={2}
+                data={images}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => {
+                  return (
+                    <PhotoTile
+                      item={item}
+                      // navigation={navigation}
+                      deviceWidth={deviceWidth}
+                      oneItem={false}
+                    />
+                  );
+                }}
+              />
+            )}
+          </View>
+        </>
       )}
     </View>
   );
