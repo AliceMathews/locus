75f85de7f8fc68e23eb769d4b08472679d3ff038
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-21 16:12:34 -0800

    cleaning up code

--- src/components/screens/upload/UploadScreen.js
+++ src/components/screens/upload/UploadScreen.js
@@ -31,7 +31,6 @@ import CustomButton from "../../global/Button";
 import FadeInView from "../../global/FadeInView";
 import { MaterialIcons } from "@expo/vector-icons";
 
-import * as ImageManipulator from "expo-image-manipulator";
 import resizeImage from "../../../helpers/upload/resizeImage";
 import generateFileName from "../../../helpers/upload/generateFileName";
 
@@ -85,17 +84,16 @@ export default function UploadScreen() {
     }
   }, [selectedImage]);
 
-  const pickImage = () => {
+  const openLibrary = () => {
     const options = {
       permissions: ImagePicker.requestCameraRollPermissionsAsync,
       launch: ImagePicker.launchImageLibraryAsync
     };
 
-    pickImage1(options);
+    pickImage(options);
   };
 
-  const pickImage1 = async options => {
-    // const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+  const pickImage = async options => {
     const permissionResult = await options.permissions();
 
     if (permissionResult.granted === false) {
@@ -123,66 +121,13 @@ export default function UploadScreen() {
     }
   };
 
-  // const resizeImage = pickerResult => {
-  //   const downsizeRatio = pickerResult.width / 1080;
-  //   const resizedDimension = {
-  //     width: 1080,
-  //     height: pickerResult.height / downsizeRatio
-  //   };
-
-  //   const manipResult = ImageManipulator.manipulateAsync(
-  //     pickerResult.uri,
-  //     [{ resize: resizedDimension }],
-  //     { compress: 0.5, format: ImageManipulator.SaveFormat.JPEG }
-  //   );
-  //   return manipResult;
-  // };
-
   const openCamera = async () => {
     const options = {
       permissions: ImagePicker.requestCameraPermissionsAsync,
       launch: ImagePicker.launchCameraAsync
     };
 
-    pickImage1(options);
-
-    //   const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
-
-    //   if (permissionResult.granted === false) {
-    //     alert("Permission to access camera is required!");
-    //     return;
-    //   }
-
-    //   try {
-    //     let pickerResult = await ImagePicker.launchCameraAsync({
-    //       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    //       exif: true
-    //     });
-
-    //     //Resize the image to width 1080, while keeping the original aspect ratio
-
-    //     const downsizeRatio = pickerResult.width / 1080;
-    //     const resizedDimension = {
-    //       width: 1080,
-    //       height: pickerResult.height / downsizeRatio
-    //     };
-
-    //     const manipResult = await ImageManipulator.manipulateAsync(
-    //       pickerResult.uri,
-    //       [{ resize: resizedDimension }],
-    //       { compress: 0.5, format: ImageManipulator.SaveFormat.JPEG }
-    //     );
-    //     if (pickerResult.cancelled === true) return;
-
-    //     setSelectedImage({
-    //       localUri: manipResult.uri,
-    //       exif: pickerResult.exif,
-    //       type: pickerResult.type
-    //     });
-    //     setMode("IMAGE");
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
+    pickImage(options);
   };
 
   const saveImage = () => {
@@ -211,29 +156,6 @@ export default function UploadScreen() {
       });
   };
 
-  // const generateFileName = () => {
-  //   let count = 0;
-  //   let randomString = "";
-
-  //   while (count < 6) {
-  //     let randomNumber = Math.floor(Math.random() * (122 - 48 + 1) + 48);
-  //     let randomChar = "";
-
-  //     if (randomNumber >= 58 && randomNumber <= 64) {
-  //       continue;
-  //     } else if (randomNumber >= 91 && randomNumber <= 96) {
-  //       continue;
-  //     } else {
-  //       //convert to character
-  //       randomChar = String.fromCharCode(randomNumber);
-  //       randomString += randomChar;
-  //       count++;
-  //     }
-  //   }
-
-  //   return `${randomString}.jpg`;
-  // };
-
   const removeTag = tagName => {
     setTags(tags.filter(tag => tag.name !== tagName));
   };
@@ -251,7 +173,7 @@ export default function UploadScreen() {
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.top}>
           {mode === "EMPTY" && (
-            <Empty press={pickImage} pressCam={openCamera} />
+            <Empty press={openLibrary} pressCam={openCamera} />
           )}
           {mode !== "EMPTY" && (
             <FadeInView duration={1000}>
