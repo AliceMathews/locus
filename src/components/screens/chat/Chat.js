8e68431db31925fe5da88264d5370815611ad542
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-22 20:40:19 -0800

    changed url

--- src/components/screens/chat/Chat.js
+++ src/components/screens/chat/Chat.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect, Component } from "react";
-import { Text, View, Image, TouchableOpacity, ImageBackground, TextInput, FlatList, StyleSheet, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  Dimensions
+} from "react-native";
 
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
-
-import { API_URL } from '../../../../configKeys';
-import styles from './ChatStyle';
+import { API_URL } from "../../../../configKeys";
+import styles from "./ChatStyle";
 import { ThemeProvider } from "@react-navigation/native";
 
 // const socket = io("https://826a3840.ngrok.io");
@@ -17,7 +26,6 @@ import { ThemeProvider } from "@react-navigation/native";
 //   const [message, setMessage] = useState("");
 //   const [chatMessages, setChatMessages] = useState([]);
 //   // const [socket, setSocket] = useState(null);
-
 
 //   useEffect(() => {
 //     // const newSocket = io("https://826a3840.ngrok.io");
@@ -32,13 +40,12 @@ import { ThemeProvider } from "@react-navigation/native";
 //       setChatMessages([...chatMessages, msg]);
 //       console.log(`message received in front end ${msg}`);
 //       console.log(chatMessages);
-      
+
 //     });
 //   },[]);
 
 //   // console.log(socket);
 
-  
 //   // console.log(chatMessages);
 
 //   const onSubmitEditing = () => {
@@ -78,53 +85,55 @@ export default class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       chatMessage: "",
-       chatMessages: []
+      chatMessage: "",
+      chatMessages: []
     };
     this.deviceWidth = Dimensions.get("window").width;
     this.room = "abc123";
     this.imageId = props.route.params.imageId;
-    console.log(`image: ${this.imageId}`)
+    console.log(`image: ${this.imageId}`);
   }
 
   componentDidMount() {
     this.socket = io("https://076b68fe.ngrok.io");
     this.socket.on("connect", () => {
-      this.socket.emit('room', this.imageId);
-    })
-    this.socket.on("chat message", msg => {
-          this.setState({ chatMessages: [...this.state.chatMessages, msg]});
+      this.socket.emit("room", this.imageId);
     });
-  } 
+    this.socket.on("chat message", msg => {
+      this.setState({ chatMessages: [...this.state.chatMessages, msg] });
+    });
+  }
 
   submitChatMessage() {
-    this.socket.emit('chat message', this.state.chatMessage);
-    this.setState({chatMessage: ''});
+    this.socket.emit("chat message", this.state.chatMessage);
+    this.setState({ chatMessage: "" });
   }
 
   render() {
     const chatMessages = this.state.chatMessages.map(chatMessage => (
-      <Text style={{borderWidth: 2, fontSize: 20}}>{chatMessage}</Text>
+      <Text style={{ borderWidth: 2, fontSize: 20 }}>{chatMessage}</Text>
     ));
 
     return (
       <View style={styles.container}>
-        <View style={styles.messagesContainer}>
-          {chatMessages}
-        </View>
+        <View style={styles.messagesContainer}>{chatMessages}</View>
         <View style={styles.inputContainer}>
           <TextInput
-            style={{height: 40, borderWidth: 2, position: 'absolute', width: this.deviceWidth * 0.9}}
+            style={{
+              height: 40,
+              borderWidth: 2,
+              position: "absolute",
+              width: this.deviceWidth * 0.9
+            }}
             autoCorrect={false}
             value={this.state.chatMessage}
             onSubmitEditing={() => this.submitChatMessage()}
             onChangeText={chatMessage => {
-              this.setState({chatMessage});
+              this.setState({ chatMessage });
             }}
           />
         </View>
       </View>
     );
   }
-
 }
