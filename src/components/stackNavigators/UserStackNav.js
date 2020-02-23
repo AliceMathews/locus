e0e44ee2fce866db2984d7bc9b1544f6f960cf7c
Jay Zhou <hollowic@hotmail.com>
2020-02-23 07:27:53 -0800

    added header logo png in assets and using them on home and profile pages

--- src/components/stackNavigators/UserStackNav.js
+++ src/components/stackNavigators/UserStackNav.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Image, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import header from "../../../assets/header.png";
 import ProfileScreen from "../screens/user/ProfileScreen";
 import LoginScreen from "../screens/user/LoginScreen";
 // import RegisterScreen from "../screens/user/RegisterScreen";
@@ -11,7 +12,23 @@ export default function UserStackNav({ authContext, state }) {
   return (
     <UserStack.Navigator>
       {state.userToken ? (
-        <UserStack.Screen name="Profile">
+        <UserStack.Screen
+          name="Profile"
+          options={{
+            headerTitle: (
+              <View>
+                <Image
+                  source={header}
+                  style={{
+                    alignSelf: "center",
+                    resizeMode: "contain",
+                    flex: 1
+                  }}
+                />
+              </View>
+            )
+          }}
+        >
           {() => (
             <ProfileScreen
               signOut={authContext.signOut}
