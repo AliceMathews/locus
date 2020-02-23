d5a410afea1016a28a133d68a5ef66a27d78058e
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-23 12:20:14 -0800

    removed console log

--- src/components/screens/user/ProfileScreen.js
+++ src/components/screens/user/ProfileScreen.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Image, Button, ActivityIndicator, FlatList, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  ActivityIndicator,
+  FlatList,
+  Dimensions
+} from "react-native";
 import styles from "./ProfileScreenStyle";
 import { API_URL } from "../../../../configKeys";
 import axios from "axios";
 
-import PhotoTile from '../home/PhotoTile';
+import PhotoTile from "../home/PhotoTile";
 
-import UserPhotoTile from './UserPhotoTile';
-
+import UserPhotoTile from "./UserPhotoTile";
 
 export default function ProfileScreen({ signOut, user, token }) {
   const [currentUser, setCurrentUser] = useState(null);
@@ -16,46 +23,46 @@ export default function ProfileScreen({ signOut, user, token }) {
   const [oneItem, setOneItem] = useState(false);
 
   useEffect(() => {
-    axios.get(`${API_URL}users/myinfo`, {headers: {"authorization": token}}).then(res => {
-      setCurrentUser(res.data);
-      const userId = res.data.id;
-      axios.get(`${API_URL}users/${userId}/images`).then(res => {
-        setImages(res.data);
-        setLoading(false);
-        if (res.data.length === 1) {
-          setOneItem(true);
-        } else {
-          setOneItem(false);
-        }
+    axios
+      .get(`${API_URL}users/myinfo`, { headers: { authorization: token } })
+      .then(res => {
+        setCurrentUser(res.data);
+        const userId = res.data.id;
+        axios.get(`${API_URL}users/${userId}/images`).then(res => {
+          setImages(res.data);
+          setLoading(false);
+          if (res.data.length === 1) {
+            setOneItem(true);
+          } else {
+            setOneItem(false);
+          }
+        });
       });
-    });
   }, []);
-  console.log(images);
+  // console.log(images);
 
   const deviceWidth = Dimensions.get("window").width;
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       {currentUser && (
         <>
           <View style={styles.profileInfoContainer}>
             <Image
-              source={{uri: currentUser.profile_pic}}
-              style={{width: 80, height: 80}} 
+              source={{ uri: currentUser.profile_pic }}
+              style={{ width: 80, height: 80 }}
             />
-            <Text style={{textAlign: 'center'}}>{currentUser.username}</Text>
+            <Text style={{ textAlign: "center" }}>{currentUser.username}</Text>
             <Button title="Sign out" onPress={signOut}></Button>
           </View>
           <View style={styles.userPhotosContainer}>
-            {loading && (
-              <ActivityIndicator size="large" color="#0000ff" />
-            )}
+            {loading && <ActivityIndicator size="large" color="#0000ff" />}
             {!loading && (
               <FlatList
                 numColumns={2}
                 data={images}
                 keyExtractor={item => item.id}
-                renderItem={({item}) => {
+                renderItem={({ item }) => {
                   return (
                     <UserPhotoTile
                       item={item}
