0dac0a35080a2c43dc209000af098313abaef0f3
edison-cy-yang <edison.cy.yang@gmail.com>
2020-02-22 17:54:34 -0800

    Can send and receive messages, right now everyone will get the message

--- src/components/screens/chat/Chat.js
+++ src/components/screens/chat/Chat.js
@@ -1,56 +1,123 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, Image, TouchableOpacity, ImageBackground, TextInput, FlatList } from "react-native";
+import React, { useState, useEffect, Component } from "react";
+import { Text, View, Image, TouchableOpacity, ImageBackground, TextInput, FlatList, StyleSheet, Dimensions } from "react-native";
 
 import io from 'socket.io-client';
 
 
-import styles from './ChatStyle';
 import { API_URL } from '../../../../configKeys';
+import styles from './ChatStyle';
 
-export default function Chat({ navgiation, route }) {
-  const { sendToId } = route.params;
-  console.log(sendToId);
+// const socket = io("https://826a3840.ngrok.io");
 
-  const [message, setMessage] = useState("");
-  const [chatMessages, setChatMessages] = useState([]);
+// export default function Chat({ navgiation, route }) {
+//   const { sendToId } = route.params;
+//   // console.log(sendToId);
+
+//   const [message, setMessage] = useState("");
+//   const [chatMessages, setChatMessages] = useState([]);
+//   // const [socket, setSocket] = useState(null);
 
 
-  useEffect(() => {
-    const socket = io("https://826a3840.ngrok.io");
-  });
+//   useEffect(() => {
+//     // const newSocket = io("https://826a3840.ngrok.io");
+//     // setSocket(newSocket);
+//     // newSocket.on("chat message", msg => {
+//     //   console.log(`message received in front end ${msg}`);
+//     //   console.log(chatMessages);
+//     //   setChatMessages([...chatMessages, msg]);
+//     // });
+
+//     socket.on("chat message", msg => {
+//       setChatMessages([...chatMessages, msg]);
+//       console.log(`message received in front end ${msg}`);
+//       console.log(chatMessages);
+      
+//     });
+//   },[]);
+
+//   // console.log(socket);
 
   
-  console.log(chatMessages);
+//   // console.log(chatMessages);
 
-  const onSubmitEditing = () => {
-    setChatMessages([...chatMessages, message]);
-    setMessage("");
+//   const onSubmitEditing = () => {
+//     // setChatMessages([...chatMessages, message]);
+//     setMessage("");
+//     socket.emit("chat message", message);
+//   }
 
+//   return (
+//     <View style={styles.container}>
+//         <View style={styles.messagesContainer}>
+//           {/* <FlatList
+//             data={chatMessages}
+//             key={1}
+//             renderItem={({ item }) => {
+//               (<Text>hi</Text>)
+//             }}
+//           /> */}
+//           {chatMessages.map((chat, idx) => {
+//             return (<Text key={idx}>{chat}</Text>);
+//           })}
+//         </View>
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             style={{height: 40, borderWidth: 2, width: 250, position: 'absolute'}}
+//             autoCorrect={false}
+//             value={message}
+//             onSubmitEditing={onSubmitEditing}
+//             onChangeText={msg => setMessage(msg)}
+//           />
+//         </View>
+//     </View>
+//   );
+// }
+
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       chatMessage: "",
+       chatMessages: []
+    };
+    this.deviceWidth = Dimensions.get("window").width;
   }
 
-  return (
-    <View style={styles.container}>
+  componentDidMount() {
+    this.socket = io("https://826a3840.ngrok.io");
+    this.socket.on("chat message", msg => {
+          this.setState({ chatMessages: [...this.state.chatMessages, msg]});
+    });
+  } 
+
+  submitChatMessage() {
+    this.socket.emit('chat message', this.state.chatMessage);
+    this.setState({chatMessage: ''});
+  }
+
+  render() {
+    const chatMessages = this.state.chatMessages.map(chatMessage => (
+      <Text style={{borderWidth: 2, fontSize: 20}}>{chatMessage}</Text>
+    ));
+
+    return (
+      <View style={styles.container}>
         <View style={styles.messagesContainer}>
-          {/* <FlatList
-            data={chatMessages}
-            key={1}
-            renderItem={({ item }) => {
-              (<Text>hi</Text>)
-            }}
-          /> */}
-          {chatMessages.map(chat => {
-            return (<Text>{chat}</Text>);
-          })}
+          {chatMessages}
         </View>
         <View style={styles.inputContainer}>
           <TextInput
-            style={{height: 40, borderWidth: 2, width: 250, position: 'absolute'}}
+            style={{height: 40, borderWidth: 2, position: 'absolute', width: this.deviceWidth * 0.9}}
             autoCorrect={false}
-            value={message}
-            onSubmitEditing={onSubmitEditing}
-            onChangeText={msg => setMessage(msg)}
+            value={this.state.chatMessage}
+            onSubmitEditing={() => this.submitChatMessage()}
+            onChangeText={chatMessage => {
+              this.setState({chatMessage});
+            }}
           />
         </View>
-    </View>
-  );
+      </View>
+    );
+  }
+
 }
