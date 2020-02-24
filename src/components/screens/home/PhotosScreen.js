f964b402b2d3b7ec46d3287f4f90a1b742d8ba72
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-24 12:46:15 -0800

    added button to sort images by proximity

--- src/components/screens/home/PhotosScreen.js
+++ src/components/screens/home/PhotosScreen.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, Button, FlatList, Dimensions, Image, ActivityIndicator } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  FlatList,
+  Dimensions,
+  Image,
+  ActivityIndicator
+} from "react-native";
 import { Tile } from "react-native-elements";
+import useCurrentLocation from "../../../hooks/useCurrentLocation";
+import { getDistance, convertSpeed } from "geolib";
+import CustomButton from "../../global/Button";
 
 import axios from "axios";
 
 import styles from "./PhotosScreenStyle";
 import { API_URL } from "../../../../configKeys";
 
-import PhotoTile from './PhotoTile';
+import PhotoTile from "./PhotoTile";
 
 export default function PhotosScreen({ route, navigation }) {
   const { categoryId } = route.params;
@@ -17,10 +28,19 @@ export default function PhotosScreen({ route, navigation }) {
   const [refreshing, setRefreshing] = useState(false);
 
   const [oneItem, setOneItem] = useState(false);
+  const { currentLocation, _getLocationAsync } = useCurrentLocation();
 
+  // useEffect(() => {
+  //   _getLocationAsync();
+  // }, [images]);
 
   useEffect(() => {
+    _getLocationAsync();
     axios.get(`${API_URL}categories/${categoryId}/images`).then(res => {
+      // res.data.images.forEach(image => {
+      //   image.distance = distance(image);
+      // });
+      console.log(res.data.images);
       setImages(res.data.images);
       setLoading(false);
       if (res.data.images.length === 1) {
@@ -42,34 +62,60 @@ export default function PhotosScreen({ route, navigation }) {
     });
   };
 
+  const distance = image => {
+    console.log(currentLocation);
+    const result = getDistance(
+      {
+        latitude: image.latitude,
+        longitude: image.longitude
+      },
+      currentLocation,
+      1
+    );
+
+    return result;
+  };
+
+  const orderByLocation = () => {
+    const imagesToSort = [...images];
+    imagesToSort.forEach(image => {
+      image.distance = distance(image);
+    });
+    imagesToSort.sort((a, b) => a.distance - b.distance);
+    console.log("SORTED");
+    console.log(imagesToSort);
+    setImages(imagesToSort);
+  };
+
   const deviceWidth = Dimensions.get("window").width;
 
   return (
     <View style={styles.container}>
       <View style={styles.bannerContainer}>
-        <Text style={styles.categoryTitle}>Photos of {route.params.categoryName}</Text>
+        <Text style={styles.categoryTitle}>
+          Photos of {route.params.categoryName}
+        </Text>
+        <CustomButton onPress={() => orderByLocation()}>Closest</CustomButton>
       </View>
       <View style={styles.photosContainer}>
-        {loading && (
-          <ActivityIndicator size="large" color="#0000ff" />
-        )}
+        {loading && <ActivityIndicator size="large" color="#0000ff" />}
         {!loading && (
           <FlatList
             numColumns={2}
             data={images}
             renderItem={({ item }) => (
               <PhotoTile
-                  item={item}
-                  navigation={navigation}
-                  deviceWidth={deviceWidth}
-                  onlyOne={oneItem}
-                  token={route.params.token}
+                item={item}
+                navigation={navigation}
+                deviceWidth={deviceWidth}
+                onlyOne={oneItem}
+                token={route.params.token}
               />
             )}
             refreshing={refreshing}
             onRefresh={onRefresh}
           />
-        )}  
+        )}
       </View>
     </View>
   );
