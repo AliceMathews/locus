28591a19d08324b707de2a14c83168c6e69ed8c7
Jay Zhou <hollowic@hotmail.com>
2020-02-24 23:55:58 -0800

    grid gap within the photos screen

--- src/components/screens/home/PhotoTile.js
+++ src/components/screens/home/PhotoTile.js
@@ -29,6 +29,7 @@ export default function PhotoTile(props) {
           key={props.item.id}
           source={{ uri: props.item.url, cache: "force-cache" }}
           style={{
+            margin: 2,
             width: props.deviceWidth / 2,
             height: props.deviceWidth / 2,
             ...styles.image
